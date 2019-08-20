0b02874a9d8998d96afff9cc1af4264aba81c626
edfortune <edmael.fortunex@gmail.com>
2019-08-20 00:11:29 -0400

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import * as faker from 'faker';
+
+class Contact {
+  avatar: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-dropdown-autocomplete';
+export class AppComponent implements OnInit {
+
+
+  openDrop: boolean = false;
+  selectedItem: Contact;
+  contacts: Contact[] = [];
+  filterContacts: Contact[] = []
+  constructor() { }
+
+  ngOnInit(): void {
+
+    for (let index = 0; index < 10; index++) {
+      const contact = new Contact();
+      contact.avatar = faker.internet.avatar();
+      contact.name = faker.name.findName();
+      contact.phone = faker.phone.phoneNumber();
+      contact.email = faker.internet.email();
+      contact.address = faker.address.streetAddress();
+      this.contacts.push(contact);
+    }
+    this.filterContacts = this.contacts;
+  }
+
+  onOpenDrop(state) {
+    this.openDrop = state;
+  }
+
+  onSelectItem(contact: Contact) {
+    this.selectedItem = contact;
+  }
+
+  onSearchContact(value: string) {
+    value = (value || '').toLowerCase();
+
+    this.filterContacts = this.contacts.filter(c => {
+      const name = c.name.toLowerCase();
+      const email = c.email.toLowerCase();
+      return name.includes(value) || email.includes(value);
+    });
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClickOutsideModule } from 'ng-click-outside';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClickOutsideModule
   ],
   providers: [],
   bootstrap: [AppComponent]
